3767b61249ce27e8e68b1b1949d0f66c0ed61c87
Kevin J Bai <897840029@qq.com>
2019-07-02 17:08:03 +0800

    使用 normalize.css  && scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './containers/app/App';
+import './index.css'
 import store from './redux/store'
 
 
