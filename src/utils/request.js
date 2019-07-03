661a8f8268c3dd9c02c42c7c55ab03babd743ec3
897840029 <897840029@qq.com>
2019-07-03 09:11:14 +0800

    封装网络请求fetch, mock数据获取

---
+++ src/utils/request.js
@@ -0,0 +1,42 @@
+const headers = new Headers({
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+})
+
+function get(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: headers
+  }).then(res => {
+    handleResponse(url, res)
+  }).catch(err =>{
+    console.error(`request failed. Url=${url}, Message=${err}`)
+    return Promise.reject({err: {message: `failed request`}})
+  })
+}
+
+function post(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body:data
+  }).then(res => {
+    handleResponse(url, res)
+  }).catch(err =>{
+    console.error(`request failed. Url=${url}, Message=${err}`)
+    return Promise.reject({err: {message: `failed request`}})
+  })
+}
+
+
+function handleResponse(url, res) {
+  if (res.status === 200) {
+    return res.json()
+  } else {
+    console.error(`request failed. Url = ${url}`)
+    return Promise.reject({err: {message: `failed request due to server error`}})
+  }
+}
+
+
+export {get, post}
