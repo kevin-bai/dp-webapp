c8f04d53105eeee3ea0463048aa812e4cb3e8d43
897840029 <897840029@qq.com>
2019-07-07 21:27:03 +0800

    feat: Category轮播组件，react-slick库

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import Home from '../home'
 import ErrorToast from '../../components/ErrorToast'
 import { actions as appActions, getError } from "../../redux/modules/app";
 import './style.scss'
 
-
-function App() {
-  const {error, appActions: {clearError}} = this.props
-  return (
-    <div className="App">
-      {error ? <ErrorToast msg={error} clearError={clearError}></ErrorToast> : null}
-      <header className="App-header">
-        Learn React
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const {error, appActions: {clearError}} = this.props
+    return (
+      <div className="App">
+        <Home></Home>
+        {error ? <ErrorToast msg={error} clearError={clearError}></ErrorToast> : null}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, props) => {
---
+++ src/containers/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Category from './components/Category'
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Category></Category>
+      </div>
+    );
+  }
+}
+
+export default Index;
