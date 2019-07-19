ed949806c19702f15f56e950d8d46407cbccf5f8
897840029 <897840029@qq.com>
2019-07-19 16:25:21 +0800

    fix: 修复redux dev tools 拼写错误

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,14 +5,17 @@ import rootReducer from './modules'
 
 let store;
 
-if (process.env.NODE_ENV !== 'production' &&
-  window.__REDUX_DEVTOOLS_EXTENTIONS__) {
+if (process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION__) {
+  console.log('dev mode')
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk,api)
   ))
 } else {
+  console.log('production mode')
   store = createStore(rootReducer, applyMiddleware(thunk,api));
 }
+
 
 export default store;
